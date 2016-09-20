b44756f557f10472db110e5ac30f08729088e9e4
Renat Blik <rblick@bk.ru>
2016-09-21 01:11:21 +0300

    HW2 + Optional - SNAPSHOT13

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -2,8 +2,8 @@ package ru.javawebinar.topjava;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealServiceImpl;
-import ru.javawebinar.topjava.web.user.AdminRestController;
 
 import java.util.Arrays;
 
@@ -16,17 +16,10 @@ public class SpringMain {
         // java 7 Automatic resource management
         try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            //will show two users
-//            adminUserController.getAll().forEach(System.out::println);
             MealServiceImpl mealService = appCtx.getBean(MealServiceImpl.class);
-
-
-//            MealRestController mealRestController = appCtx.getBean(MealRestController.class);
-//            mealRestController.save(new Meal(LocalDateTime.now(), "Borsh", 1000, 1));
-
-
-//            mealRestController.getAll(1).forEach(System.out::println);
+//            mealService.get(1, 2);
+            Meal meal = mealService.get(1, 1);
+            mealService.save(meal, 2); //throws exception
         }
     }
 }
