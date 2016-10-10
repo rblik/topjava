0018185c4ec1dffd4a9dedac6df0385605c940b4
Renat Blik <rblick@bk.ru>
2016-10-10 19:40:16 +0300

    HW5-Optional-1-1

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcHSQLDBMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcHSQLDBMealRepositoryImpl.java
@@ -2,17 +2,12 @@ package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
-import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.repository.MealRepository;
 
 import javax.sql.DataSource;
 import java.time.LocalDateTime;
@@ -22,22 +17,12 @@ import java.util.List;
 
 @Repository
 @Profile(Profiles.HSQLDB)
-public class JdbcHSQLDBMealRepositoryImpl implements MealRepository{
+public class JdbcHSQLDBMealRepositoryImpl extends AbstractJdbcMealRepositoryImpl{
     private static final RowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    @Autowired
-    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-
-    private SimpleJdbcInsert insertMeal;
-
-    @Autowired
     public JdbcHSQLDBMealRepositoryImpl(DataSource dataSource) {
-        this.insertMeal = new SimpleJdbcInsert(dataSource)
-                .withTableName("meals")
-                .usingGeneratedKeyColumns("id");
+        super(dataSource);
     }
 
     @Override
@@ -62,24 +47,6 @@ public class JdbcHSQLDBMealRepositoryImpl implements MealRepository{
             }
         }
         return meal;
-    }
-
-    @Override
-    public boolean delete(int id, int userId) {
-        return jdbcTemplate.update("DELETE FROM meals WHERE id=? AND user_id=?", id, userId) != 0;
-    }
-
-    @Override
-    public Meal get(int id, int userId) {
-        List<Meal> meals = jdbcTemplate.query(
-                "SELECT * FROM meals WHERE id = ? AND user_id = ?", ROW_MAPPER, id, userId);
-        return DataAccessUtils.singleResult(meals);
-    }
-
-    @Override
-    public List<Meal> getAll(int userId) {
-        return jdbcTemplate.query(
-                "SELECT * FROM meals WHERE user_id=? ORDER BY date_time DESC", ROW_MAPPER, userId);
     }
 
     @Override
