f0c8e1adadac0fdbea774708b3cfed05c80b0940
Renat Blik <rblick@bk.ru>
2016-10-23 22:38:39 +0300

    HW7-0

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,11 +1,11 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * GKislin
  * 06.03.2015.
  */
-@Controller
+@RestController
 public class MealRestController extends AbstractMealController {
 }
