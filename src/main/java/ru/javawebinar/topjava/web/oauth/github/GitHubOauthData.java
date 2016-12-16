d392864f2b84ebbe06c938f7f0a02f1b92030dc4
Renat Blik <rblick@bk.ru>
2016-12-16 17:28:17 +0200

    fix_oauth

--- src/main/java/ru/javawebinar/topjava/web/oauth/github/GitHubOauthData.java
+++ src/main/java/ru/javawebinar/topjava/web/oauth/github/GitHubOauthData.java
@@ -7,6 +7,6 @@ public class GitHubOauthData {
     protected static final String GET_LOGIN_URL = "https://api.github.com/user";
     protected static final String CLIENT_ID = "53cbe0812f6d580c9c2e";
     protected static final String CLIENT_SECRET = "6db047074c0960ed716b3247c53be9e16d86c390";
-    protected static final String REDIRECT_URI = "http://localhost:8080/topjava/oauth/github/callback";
+    protected static final String REDIRECT_URI = "http://rblik-topjava.herokuapp.com/oauth/github/callback";
     protected static final String CODE = "topjava_csrf_token_auth";
 }
